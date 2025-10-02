8817c980e60ebf4030342a7d235d236cd97ab40f
jlelis <jlelis@stratpoint.com>
2025-10-02 19:54:20 +0800

    style(HomePage) Adjust layout and spacing for improved aesthetics
    Adjusted the layout and spacing of the HomePage component to
    improve the overall aesthetics and visual appeal.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,11 @@ import FeaturesGrid from '@/components/features/auth/home/features-grid';
 const HomePage = () => {
   return (
     <div className="relative min-h-screen flex flex-col bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-100 dark:from-slate-950 dark:via-slate-900 dark:to-slate-800">
-      <div className="w-full max-w-6xl m-auto">
+      <div className="w-full max-w-6xl mx-auto mt-10">
         <ModeToggle />
       </div>
 
-      <main className="flex flex-col items-center justify-center flex-grow gap-8 sm:p-20">
+      <main className="relative flex flex-col items-center justify-center flex-grow gap-8 sm:p-10">
         <Hero />
         <CtaButtons />
         <FeaturesGrid />
