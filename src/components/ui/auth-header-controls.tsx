dde1698b1ff85cc0f28d69003f5feca1e341b680
jlelis <jlelis@stratpoint.com>
2025-10-03 23:41:32 +0800

    feat(AuthHeaderControls) Add style to HomeIcon
    Adds shadow, border, rounded corners, padding, and background color
    to the HomeIcon for better visual appearance.

--- src/components/ui/auth-header-controls.tsx
+++ src/components/ui/auth-header-controls.tsx
@@ -7,7 +7,10 @@ const AuthHeaderControls = () => {
     <div className="flex place-items-center gap-2">
       <ModeToggle />
       <Link href={'/'}>
-        <HomeIcon />
+        <HomeIcon
+          size={35}
+          className="shadow-sm border-1 rounded-sm p-2 bg-gray-50 hover:bg-accent dark:bg-accent"
+        />
       </Link>
     </div>
   );
