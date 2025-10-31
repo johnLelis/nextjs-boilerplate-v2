3720ed73278cfb1fdfddb8c452d2574c267c243c
jlelis <jlelis@stratpoint.com>
2025-10-31 17:01:52 +0800

    chore(eslint): simplify eslint config and update dependencies
    Refactors the eslint configuration to use the `eslint-config-next/core-web-vitals` directly instead of extending using FlatCompat. Also updates eslint and related dependencies in package.json and package-lock.json.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,10 @@
-import { FlatCompat } from "@eslint/eslintrc";
+import nextVitals from "eslint-config-next/core-web-vitals";
 import prettier from "eslint-config-prettier";
 import boundaries from "eslint-plugin-boundaries";
 import checkFile from "eslint-plugin-check-file";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextVitals,
   {
     ignores: [
       "node_modules/**",
